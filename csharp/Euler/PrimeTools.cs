91aebb3ba6e034abf6d0ad51ef4b222d4a6a4956
Nelson Wells <nelsonwellswku@gmail.com>
2013-04-26 00:49:17 -0500

    Sieve of Eratosthenes in csharp project

---
+++ csharp/Euler/PrimeTools.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler
+{
+	public class PrimeTools
+	{
+		/// <summary>
+		/// Find all prime numbers less than n
+		/// </summary>
+		/// <param name="n"></param>
+		/// <returns>IEnumerable of prime numbers</returns>
+		public IEnumerable<int> Sieve(int n)
+		{
+			var dictionary = new Dictionary<int, MarkedStatus>();
+			for(int i = 2; i < n; i++)
+			{
+				dictionary[i] = MarkedStatus.Unmarked;
+			}
+
+			int p = 2;
+			while (p*p < n)
+			{
+				dictionary = MarkForP(dictionary, p, n);
+				p = dictionary.Where(x => x.Key > p && x.Value == MarkedStatus.Unmarked).FirstOrDefault().Key;
+			}
+
+			return dictionary.Where(x => x.Value == MarkedStatus.Unmarked).Select(x => x.Key);
+		}
+
+		private Dictionary<int, MarkedStatus> MarkForP(Dictionary<int, MarkedStatus> dictionary, int p, int max)
+		{
+			for (int pp = p + p; pp < max; pp += p)
+			{
+				dictionary[pp] = MarkedStatus.Marked;
+			}
+
+			return dictionary;
+		}
+
+		private enum MarkedStatus
+		{
+			Unmarked,
+			Marked
+		}
+	}
+}
---
+++ csharp/EulerTests/PrimeToolsTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+using Euler;
+
+namespace EulerTests
+{
+	[TestClass]
+	public class PrimeToolsTest
+	{
+		[TestMethod]
+		public void PrimeTools_TestSieve()
+		{
+			// Arrange
+			int[] expected = new int[] { 2, 3, 5, 7, 11 };
+			PrimeTools primeTools = new PrimeTools();
+
+			// Act
+			IEnumerable<int> actual = primeTools.Sieve(13);
+
+			// Assert
+			actual.ShouldAllBeEquivalentTo(expected);
+		}
+	}
+}
