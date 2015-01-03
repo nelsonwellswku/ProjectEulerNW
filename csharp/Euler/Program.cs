34834765331173da27a7d4f63050ab0d8b82f561
Nelson Wells <nelsonwellswku@gmail.com>
2015-01-03 00:38:59 -0600

    Added a 10 second time limit to each problem execution

--- csharp/Euler/Program.cs
+++ csharp/Euler/Program.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Euler
 {
 	class Program
 	{
+		private static TimeSpan _tenSeconds = new TimeSpan(0, 0, 10);
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Gathering problems to run...");
@@ -21,7 +25,7 @@ namespace Euler
 			var sortedDictionary = new SortedDictionary<int, IEulerProblem>(numberToInstanceDict);
 			Console.WriteLine("Problems gathered.\n");
 
-			foreach(var entry in sortedDictionary)
+			foreach (var entry in sortedDictionary)
 			{
 				WriteIt(entry.Key, entry.Value.Solve);
 			}
@@ -32,10 +36,23 @@ namespace Euler
 
 		static void WriteIt(int problemNumber, Func<object> act)
 		{
+			var cancellationSource = new CancellationTokenSource();
+			var token = cancellationSource.Token;
+
 			var stopWatch = new Stopwatch();
 			stopWatch.Start();
-			var result = act();
+			var actionTask = Task.Run(() => act(), token);
+			while (!actionTask.IsCompleted)
+			{
+				if (stopWatch.Elapsed > _tenSeconds)
+				{
+					Console.WriteLine(string.Format("Number {0} took too long. Skipping.", problemNumber));
+					cancellationSource.Cancel();
+					return;
+				}
+			}
 			stopWatch.Stop();
+			var result = actionTask.Result;
 			Console.WriteLine("The answer to number {0} is {1}. Calculation time: {2} seconds", problemNumber, result, stopWatch.Elapsed.TotalSeconds);
 		}
 	}
